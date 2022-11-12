5c56c3c142ebe3edec358502e2a755bf9ee6b39f
bethcreate <daisy_carolin>
2022-11-12 11:13:44 +0300

    first commit

--- Agricoop/urls.py
+++ Agricoop/urls.py
@@ -1,4 +1,4 @@
-"""Octagon_Africa URL Configuration
+"""Agricoop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
